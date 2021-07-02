0fd147334802df045e5a48ee1b8245890518708a
jason7531 <54910136+jason7531@users.noreply.github.com>
2021-07-02 21:17:15 +0530

    order model added

---
+++ server/models/order.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+
+const orderSchema = mongoose.Schema({
+  shippingInfo: {
+    address: {
+      type: String,
+    },
+    phoneNo: {
+      type: String,
+      required: true,
+    },
+    postalCode: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  orderItems: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Product",
+    },
+  ],
+  paymentInfo: {
+    id: {
+      type: String,
+    },
+    status: {
+      type: String,
+    },
+  },
+  paidAt: {
+    type: Date,
+  },
+  itemsPrice: {
+    type: Number,
+    default: 0.0,
+  },
+  shippingPrice: {
+    type: Number,
+    default: 0.0,
+  },
+  totalPrice: {
+    type: Number,
+    default: 0.0,
+  },
+  orderStatus: {
+    type: String,
+    default: "Processing",
+  },
+  deliveredAt: {
+    type: Date,
+  },
+});
+
+module.exports = mongoose.model("Order", orderSchema);
