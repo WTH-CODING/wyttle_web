bdc00d24f98bfb4d64dae9149a8e06095454903a
jason7531 <54910136+jason7531@users.noreply.github.com>
2021-07-02 21:24:18 +0530

    feedback model added

---
+++ server/models/feedback.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const feedbackSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    userRating: {
+      type: Number,
+    },
+    user: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "User",
+    },
+    upvote: {
+      type: Number,
+    },
+    image: {
+      url: {
+        type: String,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Feedback", feedbackSchema);
--- server/models/order.js
+++ server/models/order.js
@@ -1,65 +1,68 @@
 const mongoose = require("mongoose");
 
-const orderSchema = mongoose.Schema({
-  shippingInfo: {
-    address: {
-      type: String,
+const orderSchema = mongoose.Schema(
+  {
+    shippingInfo: {
+      address: {
+        type: String,
+      },
+      phoneNo: {
+        type: String,
+        required: true,
+      },
+      postalCode: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
     },
-    phoneNo: {
-      type: String,
-      required: true,
-    },
-    postalCode: {
-      type: String,
-      required: true,
-    },
-    country: {
-      type: String,
-      required: true,
-    },
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "User",
-  },
-  orderItems: [
-    {
+    user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
-      ref: "Product",
+      ref: "User",
     },
-  ],
-  paymentInfo: {
-    id: {
+    orderItems: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Product",
+      },
+    ],
+    paymentInfo: {
+      id: {
+        type: String,
+      },
+      status: {
+        type: String,
+      },
+    },
+    paidAt: {
+      type: Date,
+    },
+    itemsPrice: {
+      type: Number,
+      default: 0.0,
+    },
+    shippingPrice: {
+      type: Number,
+      default: 0.0,
+    },
+    totalPrice: {
+      type: Number,
+      default: 0.0,
+    },
+    orderStatus: {
       type: String,
+      default: "Processing",
     },
-    status: {
-      type: String,
+    deliveredAt: {
+      type: Date,
     },
   },
-  paidAt: {
-    type: Date,
-  },
-  itemsPrice: {
-    type: Number,
-    default: 0.0,
-  },
-  shippingPrice: {
-    type: Number,
-    default: 0.0,
-  },
-  totalPrice: {
-    type: Number,
-    default: 0.0,
-  },
-  orderStatus: {
-    type: String,
-    default: "Processing",
-  },
-  deliveredAt: {
-    type: Date,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Order", orderSchema);
