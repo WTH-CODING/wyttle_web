59a4d81117bb3419ee6a34828ff5aefd983d8ea8
jason7531 <54910136+jason7531@users.noreply.github.com>
2021-07-05 21:28:17 +0530

    register page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-import { BrowserRouter, Switch, Route} from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navigation from "./Components/Layout/NavigationBar";
 import ProductPage from "./Components/Pages/ProductPage";
 import FeedBack from "./Components/Pages/FeedBackPage";
 import HomePage from "./Components/Pages/HomePage";
 import Footer from "./Components/Layout/Footer";
 import Login from "./Components/User/Login";
+import Register from "./Components/User/Register";
 import CartPage from "./Components/Pages/CartPage";
 function App() {
   return (
@@ -17,11 +18,11 @@ function App() {
           <Route path="/product/_id" exact component={ProductPage} />
           <Route path="/leaderboard" exact component={FeedBack} />
           <Route path="/login" exact component={Login} />
+          <Route path="/signup" exact component={Register} />
           <Route path="/cart" exact component={CartPage} />
         </Switch>
         <Footer />
       </BrowserRouter>
-      
     </div>
   );
 }
--- client/src/Components/Layout/NavigationBar.js
+++ client/src/Components/Layout/NavigationBar.js
@@ -2,63 +2,64 @@ import React from "react";
 import logo from "../../Assets/Logo.jpeg";
 import { Link, withRouter } from "react-router-dom";
 
-function NavigationBar() { 
-
-  const renderer = () =>{
-    if(window.location.pathname !== "/login"){
-      return(<div id="navbar"
-      className="row"
-      style={{ background: "rgba(219, 230, 234, 0.9)", height: "6.3rem" }}
-    >
-      <div
-        className="col-md-12"
-        style={{ alignSelf: "center", paddingLeft: "10%" }}
-      >
-        <img
-          src={logo}
-          alt="Logo"
-          style={{ height: "50px", width: "50px" }}
-        ></img>
-        <Link to="/">
-          {" "}
-          <button className="nbtn" style={{ marginLeft: "2.25rem" }}>
-            Home
-          </button>
-        </Link>
-        <Link to="/product/_id">
-          <button className="nbtn">Shop</button>
-        </Link>
-        <button className="nbtn">Contact Us</button>
-        <Link to="/leaderboard">
-          <button className="nbtn">Feedback</button>
-        </Link>
-        <Link to="/login">
-          <button className="rbtn">Login</button>
-        </Link>
-        <Link to="/cart">
-        <button
-          style={{
-            float: "right",
-            padding: "0rem 1.35rem",
-            fontSize: "xx-large",
-            background: "none",
-            border: "none",
-          }}
+function NavigationBar() {
+  const renderer = () => {
+    if (
+      window.location.pathname !== "/login" &&
+      window.location.pathname !== "/signup"
+    ) {
+      return (
+        <div
+          id="navbar"
+          className="row"
+          style={{ background: "rgba(219, 230, 234, 0.9)", height: "6.3rem" }}
         >
-          <i className="fas fa-shopping-cart"></i><sup style={{color: "red"}}>50</sup>
-        </button>
-        </Link>
-      </div>
-    </div>)
-    }
-    else{
+          <div
+            className="col-md-12"
+            style={{ alignSelf: "center", paddingLeft: "10%" }}
+          >
+            <img
+              src={logo}
+              alt="Logo"
+              style={{ height: "50px", width: "50px" }}
+            ></img>
+            <Link to="/">
+              <button className="nbtn" style={{ marginLeft: "2.25rem" }}>
+                Home
+              </button>
+            </Link>
+            <Link to="/product/_id">
+              <button className="nbtn">Shop</button>
+            </Link>
+            <button className="nbtn">Contact Us</button>
+            <Link to="/leaderboard">
+              <button className="nbtn">Feedback</button>
+            </Link>
+            <Link to="/login">
+              <button className="rbtn">Login</button>
+            </Link>
+            <Link to="/cart">
+              <button
+                style={{
+                  float: "right",
+                  padding: "0rem 1.35rem",
+                  fontSize: "xx-large",
+                  background: "none",
+                  border: "none",
+                }}
+              >
+                <i className="fas fa-shopping-cart"></i>
+                <sup style={{ color: "red" }}>50</sup>
+              </button>
+            </Link>
+          </div>
+        </div>
+      );
+    } else {
       return null;
     }
-  }
-  return(
-    renderer()
-  )
-  
+  };
+  return renderer();
 }
 
 export default withRouter(NavigationBar);
