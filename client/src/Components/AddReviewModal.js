90625324137f93711b6e6343efb172d6e8d8f504
2221mb <manish.manishbhardwaj007@gmail.com>
2021-07-06 15:38:24 +0530

    "ClassName Warning  rectified"

--- client/src/Components/AddReviewModal.js
+++ client/src/Components/AddReviewModal.js
@@ -5,26 +5,26 @@ const AddReviewModal = () => {
     return (
       <>
         <div
-          class="modal fade"
+          className="modal fade"
           id="reviewModal"
-          tabindex="-1"
+          tabIndex="-1"
           aria-labelledby="reviewModalLabel"
           aria-hidden="true"
         >
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="reviewModalLabel">
-                  <i class="far fa-clipboard"></i> Add Feedback
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="reviewModalLabel">
+                  <i className="far fa-clipboard"></i> Add Feedback
                 </h5>
                 <button
                   type="button"
-                  class="btn-close"
+                  className="btn-close"
                   data-bs-dismiss="modal"
                   aria-label="Close"
                 ></button>
               </div>
-              <div class="modal-body">
+              <div className="modal-body">
                 <div className="row">
                   <div className="col-md-3">
                     <img
@@ -36,11 +36,11 @@ const AddReviewModal = () => {
                   <div className="col-md-9" style={{ alignSelf: "center" }}>
                     <p style={{ margin: "0" }}>Product Name</p>
                     <span style={{color: "#ffe600"}}>
-                      <i class="fas fa-star"></i>
-                      <i class="fas fa-star"></i>
-                      <i class="fas fa-star"></i>
-                      <i class="fas fa-star"></i>
-                      <i class="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
+                      <i className="fas fa-star"></i>
                     </span>
                   </div>
                 </div>
@@ -48,19 +48,19 @@ const AddReviewModal = () => {
                 <div className="row">
                   <form>
                     Review Title
-                    <div class="input-group input-group-sm">
+                    <div className="input-group input-group-sm">
                       <input
                         type="text"
-                        class="form-control"
+                        className="form-control"
                         placeholder="Enter  Title"
                         aria-label="Review Title"
                         aria-describedby="Review Title"
                       />
                     </div>
                     Description
-                    <div class="input-group">
+                    <div className="input-group">
                       <textarea
-                        class="form-control"
+                        className="form-control"
                         rows="5"
                         placeholder="Enter Description"
                         aria-label="Textarea"
@@ -69,8 +69,8 @@ const AddReviewModal = () => {
                   </form>
                 </div>
               </div>
-              <div class="modal-footer" style={{ alignSelf: "center" }}>
-                <button type="button" class="btn btn-success">
+              <div className="modal-footer" style={{ alignSelf: "center" }}>
+                <button type="button" className="btn btn-success">
                   Submit Feedback
                 </button>
               </div>
